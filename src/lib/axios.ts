ff88ece2767a5083c24b744a3beb55b4f2098cae
chithong17 <chithongg17@gmail.com>
2026-02-11 21:47:17 +0700

    change api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -4,10 +4,16 @@ import type { InternalAxiosRequestConfig } from "axios";
 import { toast } from "sonner";
 
 
+// const api = axios.create({
+//   baseURL: import.meta.env.MODE === 'development' ? "http://localhost:8080/fapanese/api" : "/fapanese/api",
+//   withCredentials: true,
+// });
+
 const api = axios.create({
-  baseURL: import.meta.env.MODE === 'development' ? "http://localhost:8080/fapanese/api" : "/fapanese/api",
+  baseURL: import.meta.env.MODE === 'development' ? "https://fapanese-web.onrender.com/fapanese/api" : "/fapanese/api",
   withCredentials: true,
 });
+
 
 
 //gắn access token vào req header (phải có requiresAuth: true, khi gửi req)
